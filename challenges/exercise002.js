c5882d91bd455b319c05b8c5e00a5aea19f1fb75
Gul <guljana@gmail.com>
2020-12-01 17:58:15 +0000

    implemented all functions of excercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,56 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if(person.city == "Manchester"){
+    return true
+  }
+  return false
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+     if(people <=40){
+       return 1;
+
+     }else if(people>40 && people<=80){
+
+      return 2;
+     }else if(people>80 && people<=120){
+
+      return 3;
+     }else{
+       return Math.ceil(people/40)
+     }
+
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  var count=0;
+  for(var i=0; i<arr.length; i++){
+    if(arr[i] == "sheep")
+    count++;
+  }
+     return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  console.log(person.address.postCode);
+    if(person.address.postCode.charAt(0) == 'M' ){
+       if(person.address.city != "Manchester"){
+         return false;
+       
+        }
+        return true;
+      }
+       
+    return false
+
 }
 
 module.exports = {
